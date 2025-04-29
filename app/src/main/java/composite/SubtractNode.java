c3ae3d9001a7d2d085a3bb4b7e048f36988ec337
Josh Zhou <z5591698@ad.unsw.edu.au>
2025-04-29 16:20:46 +1000

    Implemented a simple calculator using the composite pattern

---
+++ app/src/main/java/composite/SubtractNode.java
@@ -0,0 +1,21 @@
+package composite;
+
+public class SubtractNode implements CalculatorNode {
+    private CalculatorNode left;
+    private CalculatorNode right;
+
+    public SubtractNode(CalculatorNode left, CalculatorNode right) {
+        this.left = left;
+        this.right = right;
+    }
+    @Override
+    public double evaluate() {
+        return left.evaluate() - right.evaluate();
+    }
+
+    @Override
+    public String print() {
+        return left.print() + " - " + right.print();
+    }
+    
+}
---
+++ app/src/main/java/composite/ValueNode.java
@@ -0,0 +1,19 @@
+package composite;
+
+public class ValueNode implements CalculatorNode {
+    private double val;
+
+    public ValueNode(double val) {
+        this.val = val;
+    }
+
+    @Override
+    public double evaluate() {
+        return val;
+    }
+
+    @Override
+    public String print() {
+        return Double.toString(val);
+    }
+}

13c625ec2ea8625a2902ca3dd3e8f1aea994d9b3
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-03 10:23:41 +1000

    finished singleton pattern

---
+++ app/src/main/java/singleton/Singleton.java
@@ -0,0 +1,65 @@
+package singleton;
+
+// Thread-safe Singleton implementation
+public class Singleton {
+    private static Singleton instance;
+    private double value;
+    private int number;
+
+    private Singleton() {
+        value = Math.random();
+    }
+
+    // synchronized keyword acts as a lock
+    public static synchronized Singleton getInstance() {
+        // Create new instance or use existing one
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+    public synchronized double getValue() {
+        return value;
+    }
+
+    public synchronized int getNumber() {
+        return number;
+    }
+
+    public synchronized void addNumber(int amount) {
+        this.number += amount;
+    }
+
+    public static void main(String[] args) {
+        class Thread1 implements Runnable {
+            @Override
+            public void run() {
+                Singleton singleton = Singleton.getInstance();
+                System.out.println("Thread1 value: " + singleton.getValue());
+                System.out.println("Thread1 number: " + singleton.getNumber());
+                singleton.addNumber(123);
+                System.out.println("Thread1 added 123 to number! The number is now: " + singleton.getNumber());
+            }
+        }
+
+        class Thread2 implements Runnable {
+            @Override
+            public void run() {
+                Singleton singleton = Singleton.getInstance();
+                System.out.println("Thread2 value: " + singleton.getValue());
+                System.out.println("Thread2 number: " + singleton.getNumber());
+                singleton.addNumber(456);
+                System.out.println("Thread2 added 456 to number! The number is now: " + singleton.getNumber());
+            }
+        }
+
+
+        Thread t1 = new Thread(new Thread1());
+        Thread t2 = new Thread(new Thread2());
+
+        t1.start();
+        t2.start();
+    }
+}
